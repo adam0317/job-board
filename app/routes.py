8202f90f32fb79a47921d40f2d0cdacfd7330d21
adam0317 <adam@offthehighdive.com>
2019-12-18 06:42:53 -0700

    Start to work on pagination

--- app/routes.py
+++ app/routes.py
@@ -10,9 +10,18 @@ bp = Blueprint("all", __name__)
 
 @bp.route('/')
 def index():
+  page = 0
+  jobs_array = []
+  results = True
   with open('example.json') as json_file:
     jobs = json.load(json_file)
-  # github_jobs = requests.get("https://jobs.github.com/positions.json")
-  # jobs = json.loads(github_jobs.text)
   print(len(jobs))
+  # while results:
+  #   github_jobs = requests.get("https://jobs.github.com/positions.json?page={}".format(page))
+  #   if not json.loads(github_jobs.text):
+  #     results = False
+  #   else:
+  #     jobs_array.append(json.loads(github_jobs.text))
+  #     page += 1
+  #     print(len(jobs_array[page - 2]))
   return render_template('home.html', jobs=jobs)  
